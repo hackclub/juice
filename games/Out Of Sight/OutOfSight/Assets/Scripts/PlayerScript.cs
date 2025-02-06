0995494335e2544bb544cb3bb793d670154ebf0d
Ethan P <ethan.j.pope09@gmail.com>
2025-02-05 17:04:17 -0700

    Camera changes

--- games/Out Of Sight/OutOfSight/Assets/Scripts/PlayerScript.cs
+++ games/Out Of Sight/OutOfSight/Assets/Scripts/PlayerScript.cs
@@ -8,23 +8,22 @@ public class PlayerScript : MonoBehaviour
     private bool isGrounded; // To check if the player is on the ground
     private Rigidbody rb;
 
-    void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Get horizontal input (A/D keys or Left/Right arrow keys)
+        
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        // Calculate movement vector
+        
         Vector3 movement = new Vector3(horizontalInput, 0f, 0f) * moveSpeed * Time.deltaTime;
 
-        // Move the player
+        
         transform.Translate(movement);
 
-        // Check for jump input and if the player is grounded
+       
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
