713b6ca8a11ae45c019dddc7d3ed1c55de921609
SerenityUX <thomas@serenidad.app>
2025-03-08 15:46:41 -0800

    exhibit page

---
+++ site/pages/exhibit.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Exhibit() {
+  return (
+    <>
+      <Head>
+        <title>Juice Popup Cafe Exhibit</title>
+        <meta name="description" content="Popup cafe of 30 games that teenagers fro around the world made. Free exhibit with free juice" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+}
