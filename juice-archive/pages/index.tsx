644b0b5b52680ea8ebacedf1cfae744d3e6ff0ec
Johann Heuer <127251883+johannfh@users.noreply.github.com>
2025-04-06 07:25:10 +0200

    setup first css styles

--- juice-archive/pages/index.tsx
+++ juice-archive/pages/index.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className={`${jersey25.variable}`}>
       <h1>Hello, Juice Archive!</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <p>This is a Paragraph</p>
     </div>
   );
 }
